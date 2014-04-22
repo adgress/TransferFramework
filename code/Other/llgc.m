640cebff8b82fb00ca476923ec3cd6db51f5785f
kickindirty <kickindirty@gmail.com>
2014-04-21 21:45:59 -0700

    llgc

--- code/Other/llgc.m
+++ code/Other/llgc.m
@@ -1,9 +1,11 @@
-function [fu,fu_CMN] = llgc(W,fl)
-    a = .5;
+function [fu] = llgc(W,fl,alpha)
+    if nargin < 3
+        alpha = .5;
+    end
     Disq = diag(sum(W).^-.5);
     WN = Disq*W*Disq;
     I = eye(size(WN,1));
-    fu = (1-a)*inv(I-a*WN)*fl;
-    fu_CMN = fu;
+    fu = (1-alpha)*inv(I-alpha*WN)*fl;
+    fu = Helpers.normRows(fu);
 end
 
