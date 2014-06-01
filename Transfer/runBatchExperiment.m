822ba8f7c008eeb1f40caaf5e882025bbb91d30f
kickindirty <kickindirty@gmail.com>
2014-06-01 13:46:14 -0700

    fixed typo

--- Transfer/runBatchExperiment.m
+++ Transfer/runBatchExperiment.m
@@ -19,7 +19,7 @@ function [] = runBatchExperiment(multithread, dataset)
     if runMeasures
         configFiles{end+1} = 'config/measure/batchNN.cfg';
         configFiles{end+1} = 'config/measure/batchLLGC.cfg';
-        configFiles{end+1} = 'config/measue/batchHF.cfg';             
+        configFiles{end+1} = 'config/measure/batchHF.cfg';             
     end
     if runRepair
         configFiles{end+1} = 'config/repair/batchFuseLLGC.cfg';
