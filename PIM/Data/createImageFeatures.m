232ad10709b93cb2fe2598c7a48751ad907206ae
Aubrey Gress <adgress@overachiever.(none)>
2014-06-09 13:27:52 -0700

    Create Image Features changes

--- PIM/Data/createImageFeatures.m
+++ PIM/Data/createImageFeatures.m
@@ -1,29 +1,29 @@
 function [] = createImageFeatures()
-    runLaptap = 1;
+    runLaptap = 0;
     if runLaptap
         inputDir = 'C:\Users\Aubrey\Desktop\matlab\imageVectorFiles\';
-        outputDir = 'C:\Users\Aubrey\Google Drive\Research\TransferFramework\PIM\Data\featureData\';
+        outputDir = 'C:\Users\Aubrey\Google Drive\Research\TransferFramework\PIM\Data\featData\';
         allKeypoints = zeros(100000,128);
         numToUse = 10;
         numClusters = 10;
     else
         inputDir = '/home/adgress/PIM/imageVectorFiles/';
-        outputDir = 'PIM/Data/featureData/';
+        outputDir = 'PIM/Data/featData/';
         allKeypoints = zeros(1000000,128);
         numToUse = -1;
-        numClusters = 1000;
+        numClusters = 100;
     end
     allFiles = dir(inputDir);
     if ~runLaptap
         numToUse = length(allFiles);
     end
     
-    savedDataFile = [outputDir 'generatedData' num2str(numToUse) '-' num2str(numClusters) '.mat'];
+    savedDataFile = [outputDir 'generatedData' num2str(numToUse) '.mat'];
     if exist(savedDataFile)
         load(savedDataFile);
     else
         keypointToImages = zeros(size(allKeypoints,1),1);
-        fileNames = cell(590,1);
+        fileNames = cell(500,1);
         index = 1;
         matInd = 0;
         for i=1:numToUse
@@ -49,6 +49,6 @@ function [] = createImageFeatures()
     featResults.C = C;
     featResults.fileNames = fileNames;
     featResults.keyPointToImage = keypointToImages(1:matInd);
-    fileName = ['featResults' num2str(numToUse) '-' num2str(numClusters) '.mat'];
+    fileName = ['featResults' num2str(index-1) '-' num2str(numClusters) '.mat'];
     save([outputDir fileName],'featResults');
 end
