dadd359f571b2ac40cee045b46379befd01c5878
kickindirty <kickindirty@gmail.com>
2014-06-13 13:23:44 -0700

    deleted old TR results

--- Transfer/deleteResults.m
+++ Transfer/deleteResults.m
@@ -1,9 +1,6 @@
-function [] = deleteResults(fileName,dataSet,removeDirs)
+function [] = deleteResults(fileName,dataSet)
     if nargin < 2
         dataSet = Constants.CV_DATA;
-    end
-    if nargin < 3
-        removeDirs = 0;
     end
     s = getProjectConstants();
     projectDir = s.projectDir;
@@ -15,7 +12,6 @@ function [] = deleteResults(fileName,dataSet,removeDirs)
     end
     resultDirectories = ls(resultsDir);
     numDeleted = 0;
-    numDirsDeleted = 0;
     for i=1:length(resultDirectories)
         d = resultDirectories(i,:);
         if isequal(d,'.  ') || isequal(d,'.. ')
@@ -26,17 +22,8 @@ function [] = deleteResults(fileName,dataSet,removeDirs)
         if exist(currFile,'file')
             delete(currFile);
             numDeleted = numDeleted+1;
-        else
-            delete(currFile);
-            if removeDirs
-                rmdir(currFile);
-                numDirsDeleted = numDirsDeleted + 1;
-            end
-        end        
+        end
     end
     display(sprintf('Deleted %d files',numDeleted));
-    if removeDirs
-        display(sprintf('Deleted %d directories',numDirsDeleted));
-    end
 end
 
--- Transfer/deleteTransferRepairResults.m
+++ Transfer/deleteTransferRepairResults.m
@@ -1,4 +1,23 @@
 function [] = deleteTransferRepairResults()   
-    deleteResults('REP/*');
+    if nargin < 2
+        dataSet = Constants.CV_DATA;
+    end
+    s = getProjectConstants();
+    projectDir = s.projectDir;
+    resultsDir = [projectDir '/results'];
+    if dataSet == Constants.CV_DATA
+        resultsDir = [resultsDir '/CV'];
+    else
+        error('unknown dataset');
+    end
+    resultDirectories = ls(resultsDir);
+    for i=1:length(resultDirectories)
+        d = resultDirectories(i,:);
+        if isequal(d,'.  ') || isequal(d,'.. ')
+            continue;
+        end
+        toRemove = [resultsDir '/' d '/REP/'];
+        rmdir(toRemove,'s');
+    end
 end
 
