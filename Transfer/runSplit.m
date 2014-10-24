ba8c3e3d272879eea1fcce11617eb8d6f3b6ef07
Aubrey Gress <agress@ucdavis.edu>
2014-10-24 09:32:59 -0700

    removed unnecessary files

--- Transfer/runSplit.m
+++
@@ -1,9 +0,0 @@
-function [] = runSplit()
-    setPaths;
-    %o = DataSplitterConfigLoader('config/splitAmazon.cfg','config/splitCommon.cfg');
-    o = DataSplitterConfigLoader('config/split/splitTransferA2C.cfg',...
-        'config/split/splitCommon.cfg');
-    o.splitData();
-    o.saveSplit();
-    clear;
-end
