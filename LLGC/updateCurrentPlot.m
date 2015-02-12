942a4f311b9cead21c1a00f7c8c1789ad09d490d
Aubrey Gress <agress@ucdavis.edu>
2015-02-11 18:05:18 -0800

    figures for weighted LLGC paper

---
+++ LLGC/updateCurrentPlot.m
@@ -0,0 +1,31 @@
+%For Transfer performance plots
+
+set(findall(gcf,'type','text'),'FontSize',20);
+set(legend,'Location','south');
+
+%For data set weight plots
+%{
+fontSize = 25;
+subplot(1,5,1)
+ylabel('Weight');
+set(findall(gcf,'type','text'),'FontSize',fontSize);
+subplot(1,5,3)
+xlabel('Data Set Index');
+set(findall(gcf,'type','text'),'FontSize',fontSize);
+%}
+
+%For Accuracy-with-label-noise plots
+%{
+title('Label Noise: 55%');
+set(findall(gcf,'type','text'),'FontSize',25);
+set(legend,'Location','south');
+a(3) = .2;
+a(4) = 1;
+axis(a);
+%}
+
+%For label noise prediction plots
+%{
+title('Label Noise: 5%');
+set(findall(gcf,'type','text'),'FontSize',18);
+%}
