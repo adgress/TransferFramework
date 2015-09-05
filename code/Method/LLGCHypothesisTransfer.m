e84263ea7fd1d02b9829739186d757dcc1b92930
Aubrey Gress <agress@ucdavis.edu>
2015-09-04 17:13:32 -0700

    fix transfer regularization parameters

--- code/Method/LLGCHypothesisTransfer.m
+++ code/Method/LLGCHypothesisTransfer.m
@@ -185,9 +185,9 @@ classdef LLGCHypothesisTransfer < LLGCMethod
             cvParams = struct('key','values');
             cvParams(1).key = 'reg';
             %cvParams(1).values = num2cell([0 10.^(-1:3)]);
-            cvParams(1).values = num2cell([0 1 5 10]);
+            %cvParams(1).values = num2cell([0 1 5 10]);
             %cvParams(1).values = num2cell([0 1 5]);
-            %cvParams(1).values = num2cell([5]);
+            cvParams(1).values = num2cell([5]);
             %cvParams(2).key = 'sigma';
             %cvParams(2).values = num2cell(2.^(-3:3));
             cvParams(2).key = 'alpha';
